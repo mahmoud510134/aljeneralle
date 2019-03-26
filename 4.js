9549c06f952e9694f464a9cbb1ae096e36374535
AL-Jenerale <48939046+mahmoud510134@users.noreply.github.com>
2019-03-26 11:15:37 +0300

    Add files via upload

---
+++ 4.js
@@ -0,0 +1,61 @@
+Skip to content
+ 
+Search or jump to…
+
+Pull requests
+Issues
+Marketplace
+Explore
+ 
+@mahmoud510134 Sign out
+32
+278 102 lukaszfiszer/selectnav.js
+ Code  Issues 17  Pull requests 1  Projects 0  Insights
+selectnav.js/package.json
+@lukaszfiszer lukaszfiszer Added first unit tests with Karma
+538237c on 21 Feb 2014
+32 lines (31 sloc)  872 Bytes
+    
+{
+  "name": "selectnav.js",
+  "version": "0.1.0",
+  "description": "SelectNav.js is a JavaScript plugin that lets you convert your website navigation into a select drop-down menu. Used together with media queries it helps you to create a space saving, responsive navigation for small screen devices.",
+  "main": "selectnav-new.js",
+  "devDependencies": {
+    "karma": "~0.10.9",
+    "chai": "~1.9.0",
+    "mocha": "~1.17.1",
+    "karma-mocha": "~0.1.1"
+  },
+  "scripts": {
+    "test": "./node_modules/.bin/karma start --single-run"
+  },
+  "repository": {
+    "type": "git",
+    "url": ""
+  },
+  "keywords": [
+    "browser",
+    "frontend",
+    "responsive",
+    "menu",
+    "navigation"
+  ],
+  "author": "Lukasz Fiszer",
+  "license": "MIT",
+  "bugs": {
+    "url": ""
+  }
+}
+© 2019 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Help
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
